51e1d73187392b8557c03e3bdca1126ea91006c8
dipanshushora <deepanshushora@gmail.com>
2020-10-09 16:53:06 +0530

    all files deleted

--- dateModules.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var cdt = require('./cdt.js');
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    response.write(cdt.currentDateTime());
-    response.end();
-}).listen(8080);
--- hello.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    response.write("hello World");
-    response.end();
-}).listen(8080);
